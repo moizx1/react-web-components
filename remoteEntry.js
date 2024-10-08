df2960843633aaa9b2055b9ddfc8ab7dc890b403
Abdul Muiz <96680062+moizx1@users.noreply.github.com>
2024-10-08 11:46:32 +0500

    Update remoteEntry.js

--- remoteEntry.js
+++ remoteEntry.js
@@ -1,54 +1,53 @@
-const currentImports = {};
-const exportSet = /* @__PURE__ */ new Set(["Module", "__esModule", "default", "_export_sfc"]);
-let moduleMap = {
-  "./FormApp1": () => {
-    dynamicLoadingCss([], false, "./FormApp1");
-    return __federation_import("${__federation_expose_./FormApp1}").then((module) => Object.keys(module).every((item) => exportSet.has(item)) ? () => module.default : () => module);
-  }
+import { _ as d } from "./preload-helper.ea511a32.js";
+const a = {},
+  f = new Set(["Module", "__esModule", "default", "_export_sfc"]);
+let c = {
+  "./FormApp1": () => (
+    p([], !1, "./FormApp1"),
+    h("./__federation_expose_FormApp1.816449a6.js").then((e) =>
+      Object.keys(e).every((o) => f.has(o)) ? () => e.default : () => e
+    )
+  ),
 };
-const seen = {};
-const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
-  const metaUrl = import.meta.url;
-  if (typeof metaUrl == "undefined") {
-    console.warn('The remote style takes effect only when the build.target option in the vite.config.ts file is higher than that of "es2020".');
-    return;
-  }
-  const curUrl = metaUrl.substring(0, metaUrl.lastIndexOf("remoteEntry.js"));
-  cssFilePaths.forEach((cssFilePath) => {
-    const href = curUrl + cssFilePath;
-    if (href in seen)
+const l = {},
+  p = (e, o, _) => {
+    const s = import.meta.url;
+    if (typeof s > "u") {
+      console.warn(
+        'The remote style takes effect only when the build.target option in the vite.config.ts file is higher than that of "es2020".'
+      );
       return;
-    seen[href] = true;
-    if (dontAppendStylesToHead) {
-      const key = "css__form-app-1__" + exposeItemName;
-      if (window[key] == null)
-        window[key] = [];
-      window[key].push(href);
-    } else {
-      const element = document.head.appendChild(document.createElement("link"));
-      element.href = href;
-      element.rel = "stylesheet";
     }
-  });
-};
-async function __federation_import(name) {
-  currentImports[name] ??= import(name);
-  return currentImports[name];
+    const i = s.substring(0, s.lastIndexOf("remoteEntry.js"));
+    e.forEach((n) => {
+      const t = i + n;
+      if (!(t in l))
+        if (((l[t] = !0), o)) {
+          const r = "css__form-app-1__" + _;
+          window[r] == null && (window[r] = []), window[r].push(t);
+        } else {
+          const r = document.head.appendChild(document.createElement("link"));
+          (r.href = t), (r.rel = "stylesheet");
+        }
+    });
+  };
+async function h(e) {
+  return (a[e] ??= d(() => import(e), [])), a[e];
 }
-const get = (module) => {
-  if (!moduleMap[module])
-    throw new Error("Can not find remote module " + module);
-  return moduleMap[module]();
-};
-const init = (shareScope) => {
-  globalThis.__federation_shared__ = globalThis.__federation_shared__ || {};
-  Object.entries(shareScope).forEach(([key, value]) => {
-    const versionKey = Object.keys(value)[0];
-    const versionValue = Object.values(value)[0];
-    const scope = versionValue.scope || "default";
-    globalThis.__federation_shared__[scope] = globalThis.__federation_shared__[scope] || {};
-    const shared = globalThis.__federation_shared__[scope];
-    (shared[key] = shared[key] || {})[versionKey] = versionValue;
-  });
-};
-export { dynamicLoadingCss, get, init };
+const m = (e) => {
+    if (!c[e]) throw new Error("Can not find remote module " + e);
+    return c[e]();
+  },
+  b = (e) => {
+    (globalThis.__federation_shared__ = globalThis.__federation_shared__ || {}),
+      Object.entries(e).forEach(([o, _]) => {
+        const s = Object.keys(_)[0],
+          i = Object.values(_)[0],
+          n = i.scope || "default";
+        globalThis.__federation_shared__[n] =
+          globalThis.__federation_shared__[n] || {};
+        const t = globalThis.__federation_shared__[n];
+        (t[o] = t[o] || {})[s] = i;
+      });
+  };
+export { p as dynamicLoadingCss, m as get, b as init };
